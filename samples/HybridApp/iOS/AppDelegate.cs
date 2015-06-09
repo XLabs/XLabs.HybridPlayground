0b34661f579198a4147fe8a618fa75dc8f36f93d
Sami M. Kallio <skallio@Samis-MacBook-Pro-2.local>
2015-06-09 12:06:03 -0400

    Added a simple sample project

---
+++ samples/HybridApp/iOS/AppDelegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using XLabs.Ioc;
+using XLabs.Serialization;
+using XLabs.Serialization.ServiceStack;
+using XLabs.Forms.Controls;
+using XLabs.Platform.Device;
+
+namespace HybridApp.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            var web = new HybridWebViewRenderer();
+
+            var container = new SimpleContainer();
+
+            container.Register<IJsonSerializer, JsonSerializer>();
+            container.Register<IDevice>(AppleDevice.CurrentDevice);
+
+            Resolver.SetResolver(container.GetResolver());
+
+            global::Xamarin.Forms.Forms.Init();
+
+            // Code for starting up the Xamarin Test Cloud Agent
+            #if ENABLE_TEST_CLOUD
+            Xamarin.Calabash.Start();
+            #endif
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
+
